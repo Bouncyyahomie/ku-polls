971550efb37b06c873ce4794c4ca244b625797d2
Bouncyyahomie <thornny.s@gmail.com>
2020-09-18 01:26:21 +0700

    Fixed and create main page

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path
+from . import views;
 
 urlpatterns = [
+    path('', views.index, name = "main" ),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls'))
 ]
+
+
